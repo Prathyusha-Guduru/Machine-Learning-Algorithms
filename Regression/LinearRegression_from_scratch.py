f1c755d6c581177a65235c95742bdbc3d58e5fd9
Prathyusha-Guduru <prathyushaguduru28@gmail.com>
2022-02-03 12:51:46 +0530

    Updated linear reg exercise

--- Regression/LinearRegression_from_scratch.py
+++ Regression/LinearRegression_from_scratch.py
@@ -1,32 +1,43 @@
 # Importing necessary libraries
 import numpy as np
 
-x = np.array([1,2,3,4,5])
-y = np.array([11,12,13,14,15])
+# x = np.array([1,2,3,4,5])
+# y = np.array([11,12,13,14,15])
+
+# # Mean of x and y
+
+# x_mean = np.mean(x)
+# y_mean = np.mean(y)
+# xy_mean = np.mean(x*y)
+
+# #Covariance of x and y
+# cov_x_y = xy_mean - (x_mean*y_mean)
+
+# #variance of x
+# var_x = np.var(x)
+
+# #Calculating m in y=mx+c
+# m = cov_x_y/var_x
+
+# #c = y-mx
+# c = y_mean - (m * x_mean)
+
+# #value of predicted y
+# y_pred = m*x + c
+
+# #calculating mean squared error
+# mse = np.mean(np.square(y_pred-y))
+
+# print(f"X_mean = {x_mean}\nY_mean = {y_mean}\nxy_mean = {xy_mean}\nvar(x)={var_x}\nm={m}\nc={c}\ny={y}\nmse={mse}")
 
 
-# Mean of x and y
+#Linear Regression with multiple inputs and singlee output
 
-x_mean = np.mean(x)
-y_mean = np.mean(y)
-xy_mean = np.mean(x*y)
+x = np.array([[1,25,3,50],[1,25,2,60],[1,28,1,80],[1,25,0.5,59]])
+y = np.array([660,580,425,320])
 
-#Covariance of x and y
-cov_x_y = xy_mean - (x_mean*y_mean)
+x_t = np.transpose(x)
 
-#variance of x
-var_x = np.var(x)
+w = np.linalg.inv(x.T.dot(x)).dot(x.T).dot(y)
 
-#Calculating m in y=mx+c
-m = cov_x_y/var_x
-
-#c = y-mx
-c = y_mean - (m * x_mean)
-
-#value of predicted y
-y_pred = m*x + c
-
-#calculating mean squared error
-mse = np.mean(np.square(y_pred-y))
-
-print(f"X_mean = {x_mean}\nY_mean = {y_mean}\nxy_mean = {xy_mean}\nvar(x)={var_x}\nm={m}\nc={c}\ny={y}\nmse={mse}")
+print(f"weights are {w}")
